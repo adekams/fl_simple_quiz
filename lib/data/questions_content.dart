d1e222dfb474e326a7df065e8d418dcab68f4605
Adekams <kifademi@gmail.com>
2023-04-29 20:06:41 +0100

    setting up questions

---
+++ lib/data/questions_content.dart
@@ -0,0 +1,40 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questionContent = [
+  QuizQuestion(
+    'What language is used in Flutter?',
+    [
+      'Dart',
+      'Javascript',
+      'Java',
+      'Spring',
+    ],
+  ),
+  QuizQuestion(
+    'What are the main building blocks of Flutter?',
+    [
+      'Widgets',
+      'Components',
+      'Functions',
+      'Classes',
+    ],
+  ),
+  QuizQuestion(
+    'Which company built Flutter?',
+    [
+      'Google',
+      'Facebook',
+      'React',
+      'Spring',
+    ],
+  ),
+  QuizQuestion(
+    'What widget is used when there is a change in state?',
+    [
+      'Stateful Widget',
+      'Stateless Widget',
+      'Restless Widget',
+      'Restful Widget',
+    ],
+  ),
+];
